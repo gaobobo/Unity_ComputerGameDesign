10cbb8be72654f0c70ca9bc19131bbb6aa14e19a
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-07-29 20:31:30 +0800

    fix: speed not match editor

--- 4_MecanimSystem/Assets/scripts/Role_RunAndRotate.cs
+++ 4_MecanimSystem/Assets/scripts/Role_RunAndRotate.cs
@@ -31,12 +31,12 @@ public class Role_RunAndRotate : MonoBehaviour
         _animator.SetBool("isWalkUpend", verticalKey < 0);
         _animator.SetFloat("walkSpeed", System.Math.Abs(verticalKey));
 
-        player.transform.Translate(Vector3.forward * (verticalKey * speed));
+        player.transform.Translate(Vector3.forward * (verticalKey * speed * Time.deltaTime));
 
         if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Walk")
             || _animator.GetCurrentAnimatorStateInfo(0).IsName("UpendWalk")) 
         {
-            player.transform.Rotate(Vector3.up * (HorizontalKey * speed * 10));
+            player.transform.Rotate(Vector3.up * (HorizontalKey * speed * 10 * Time.deltaTime));
         }
 
     }
