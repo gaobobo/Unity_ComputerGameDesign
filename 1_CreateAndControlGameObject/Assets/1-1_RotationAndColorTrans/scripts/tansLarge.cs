063705ea7d487b5b0769f213c1de3af715e178cb
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-07-29 15:56:22 +0800

    fix: transform size wrong

--- 1_CreateAndControlGameObject/Assets/1-1_RotationAndColorTrans/scripts/tansLarge.cs
+++ 1_CreateAndControlGameObject/Assets/1-1_RotationAndColorTrans/scripts/tansLarge.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tansSmall : MonoBehaviour
+public class tansLarge : MonoBehaviour
 {
 
     public GameObject obj;
@@ -20,6 +20,6 @@ public class tansSmall : MonoBehaviour
 
     public void onClick()
     {
-        obj.transform.localScale += new Vector3(-1.1f, -1.1f, -1.1f);
+        obj.transform.localScale += new Vector3(1.1f, 1.1f, 1.1f);
     }
 }
